bdbfffc6999d46f1345adc8156276e3703d044eb
ChunWeiLai <cwl282@nyu.edu>
2012-03-24 17:42:18 -0400

    added unit test

--- test/com/secondmarket/daoimpl/TestCompanyDAOImpl.java
+++ test/com/secondmarket/daoimpl/TestCompanyDAOImpl.java
@@ -1,33 +1,20 @@
 package com.secondmarket.daoimpl;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.UnknownHostException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-import java.util.regex.Pattern;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
 
 import junit.framework.Assert;
 import junit.framework.TestCase;
-
-import org.codehaus.jackson.JsonParser;
-import org.codehaus.jackson.map.ObjectMapper;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
-import com.mongodb.DBObject;
 import com.mongodb.Mongo;
 import com.mongodb.MongoException;
-import com.mongodb.util.JSON;
-import com.secondmarket.model.Company;
-import com.secondmarket.utility.DataFilter;
+
+import com.secondmarket.daoimpl.CompanyDAOImpl;
 
 public class TestCompanyDAOImpl extends TestCase {
 
@@ -60,9 +47,9 @@ public class TestCompanyDAOImpl extends TestCase {
 			while (cursor.hasNext()) {
 				result.append(cursor.next());
 			}
-			
+
 			Assert.assertTrue(result.toString().length() != 0);
-		  
+
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		} catch (MongoException e) {
@@ -77,7 +64,7 @@ public class TestCompanyDAOImpl extends TestCase {
 			StringBuffer result = new StringBuffer();
 			for (String s : mongo.getDatabaseNames()) {
 				result.append(s);
-			//	System.out.println(s);
+				// System.out.println(s);
 			}
 
 			Assert.assertTrue(result.toString().length() != 0);
@@ -90,179 +77,117 @@ public class TestCompanyDAOImpl extends TestCase {
 
 	}
 		
-	/*public void testsaveMasterlist(List<Object> masterList) {
-		// create a new list save to the database then get the list out again
-		List<Object> testList = new ArrayList<Object>()
-		testList=testList.add(test1);
-		testList=testList.add(test2);
-		DBCollection collection  = db.getCollection("companies");
-		collection.drop();
-		BasicDBObject doc = new BasicDBObject();
-		doc.put("MasterList",testList);
-		collection.insert(doc);
-		testList=testList.removeAll(c);
-		DBObject companies = collection.findOne();
-		List<Object> testList = (List<Object>) companies.get("MasterList");
-		Assert.assertTrue(testList.toString().length()!=0);
-	}
-	*/
-	public void testsaveMasterlist(List<Object> masterList) {
+	public void testsaveMasterlist() {
 		//drop collection then call saveMasterlist then check database not null
+		try{
+		Mongo mongo = new Mongo("localhost", 27017);
+		DB db = mongo.getDB("SecondMarketTest");
 		DBCollection coll = db.getCollection("companies");
 		coll.drop();
-		saveMasterlist(List<Object> masterList);
-		List<Object> masterList = (List<Object>) companies.get("masterlist");
-		Assert.assertTrue(masterList.toString().length()!=0);
+		
+		List<Object> testList = new ArrayList<Object>();
+		testList.add("test1");
+		testList.add("test2");
+		CompanyDAOImpl c = new CompanyDAOImpl();
+		c.saveMasterlist(testList);		
+		testList=c.getMasterList();
+		Assert.assertFalse(testList.isEmpty());
+		coll.drop();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (MongoException e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 	public void testgetMasterList() {
-	    // put the getlist to testList then check length
-		List<Object> testList = new ArrayList<Object>()
-	    testList=getMasterList();
-	    Assert.assertTrue(testList.toString().length()!=0);
-	
+		try{
+			Mongo mongo = new Mongo("localhost", 27017);
+			DB db = mongo.getDB("SecondMarketTest");
+			DBCollection coll = db.getCollection("companies");
+			coll.drop();
+			
+			List<Object> testList = new ArrayList<Object>();
+			testList.add("test1");
+			testList.add("test2");
+			CompanyDAOImpl c = new CompanyDAOImpl();
+			c.saveMasterlist(testList);		
+			testList=c.getMasterList();
+			Assert.assertFalse(testList.isEmpty());
+			coll.drop();
+			
+		    } catch (UnknownHostException e) {
+				e.printStackTrace();
+			} catch (MongoException e) {
+				e.printStackTrace();
+			}
 	}
 
-	public void saveCompany(String companyName, Map<String, String> map) {
-		//看不懂ＭＡＰ ＮＯＷ
-		Company company = new Company();
-		basicDBObject = (BasicDBObject) JSON.parse(gson.toJson(map));
-		DataFilter.retrieveAndSetCompanyBasicInfo(basicDBObject, company);
-		company.setCrunchbaseDoc(map);
-		ds.save(company);
+	public void testsaveCompany(){
+			
 	}
-
-	/*
-	 * public List<Company> findAllCompanies() {
-		List<Company> rawDataList = ds.find(Company.class).asList();
-		List<Company> companyList = new ArrayList<Company>();
-
-		Iterator<Company> it = rawDataList.iterator();
-		while (it.hasNext()) {
-			Company company = it.next();
-			Map<String, String> map = company.getCrunchbaseDoc();
-			basicDBObject = (BasicDBObject) JSON.parse(gson.toJson(map));
-
-			// Filter the data and update the company model
-			DataFilter.retrieveAndSetCompanyBasicInfo(basicDBObject, company);
-
-			companyList.add(company);
-		}
-		return companyList;
-	}
-   */
-	public void testfindAllCompanies() {
-		List<Object> testList = new ArrayList<Object>();
-		testList=testfindAllCompanies();
-		Assert.assertNotNull(testList);
-	}
-	
-	public void testfindCompanyByName() {
-		String result=findCompanyByName(Facebook).companyName;
-		String expect="Facebook";
-		Assert.assertEquals(expect, result);
-		String result=findCompanyByName(Facebook).location;
-		String expect="New York";
-		Assert.assertEquals(expect, result);
-		String result=findCompanyByName(Facebook).country;
-		String expect="USA";
-		Assert.assertEquals(expect, result);
-		String result=findCompanyByName(Facebook).industry;
-		String expect="Consumer Web";
-		Assert.assertEquals(expect, result);
-	}
-	public List<Company> findCompaniesByImpreciseName(String companyName) {
-		List<Company> companyList = ds.find(Company.class).field("companyName")
-				.equal(Pattern.compile(companyName, Pattern.CASE_INSENSITIVE))
-				.asList();
-
-		Iterator<Company> it = companyList.iterator();
-		while (it.hasNext()) {
-			Company company = it.next();
-			Map<String, String> map = company.getCrunchbaseDoc();
-			basicDBObject = (BasicDBObject) JSON.parse(gson.toJson(map));
-			DataFilter
-					.retrieveAndSetCompanyDetailedInfo(basicDBObject, company);
-		}
-		return companyList;
-	}
-
-	public List<Company> findCompaniesInPage(int pageIndex,
-			int numberOfElementsPerPage) {
-		List<Company> tempList = null;
-		if (pageIndex != 0) {
-			tempList = ds.createQuery(Company.class)
-					.offset(pageIndex * numberOfElementsPerPage)
-					.limit(numberOfElementsPerPage).asList();
-		} else {
-			tempList = ds.createQuery(Company.class)
-					.limit(numberOfElementsPerPage).asList();
-		}
-
-		// Filter out basic info
-		List<Company> paginatedList = new ArrayList<Company>();
-		Iterator<Company> it = tempList.iterator();
-		while (it.hasNext()) {
-			Company company = it.next();
-			Map<String, String> map = company.getCrunchbaseDoc();
-			basicDBObject = (BasicDBObject) JSON.parse(gson.toJson(map));
-
-			// Filter the data and update the company model
-			DataFilter.retrieveAndSetCompanyBasicInfo(basicDBObject, company);
-			paginatedList.add(company);
-		}
-
-		return paginatedList;
-	}
-	public List<Company> findCompaniesInPage(int pageIndex,
-			int numberOfElementsPerPage) {
-		List testList = new ArrayList<Company>();
 		
-		return paginatedList;
-	}
-	public void TestgetPageAmount() {
+	/*public void testfindAllCompanies() {
+		try{	
 		
-		int answer=getPageAmount(10);
-		long numberOfCompanies = ds.createQuery(Company.class).countAll();
-		int expect = (int) Math.ceil(((double) numberOfCompanies) / 10);
-		Assert.assertEquals(expect, answer);
-	}
-	
-	
-	
-	public List<Company> findSortedCompaniesInPage(int pageIndex,
-			int numberOfElementsPerPage, String sortByField,
-			boolean isDescending) {
-		String orderStr = (isDescending) ? ("-" + sortByField) : sortByField;
+		Mongo mongo = new Mongo("localhost", 27017);
+		DB db = mongo.getDB("SecondMarketTest");
+		DBCollection collection = db.getCollection("company");
 
-		List<Company> tempList = null;
-		if (pageIndex != 0) {
-			tempList = ds.createQuery(Company.class).order(orderStr)
-					.offset(pageIndex * numberOfElementsPerPage)
-					.limit(numberOfElementsPerPage).asList();
-		} else {
-			tempList = ds.createQuery(Company.class).order(orderStr)
-					.limit(numberOfElementsPerPage).asList();
+		BasicDBObject doc = new BasicDBObject();
+		doc.put("companyName", "AAAA");
+		doc.put("location", "NY");
+		doc.put("country", "USA");
+		doc.put("industry", "web");
+		
+		BasicDBObject info = new BasicDBObject();
+		info.put("x", 203);
+		info.put("y", 102);
+
+		doc.put("info", info);
+		
+		collection.insert(doc);
+		
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (MongoException e) {
+			e.printStackTrace();
 		}
-
-		// Filter out basic info
-		List<Company> paginatedList = new ArrayList<Company>();
-		Iterator<Company> it = tempList.iterator();
-		while (it.hasNext()) {
-			Company company = it.next();
-			Map<String, String> map = company.getCrunchbaseDoc();
-			basicDBObject = (BasicDBObject) JSON.parse(gson.toJson(map));
-
-			// Filter the data and update the company model
-			DataFilter.retrieveAndSetCompanyBasicInfo(basicDBObject, company);
-			paginatedList.add(company);
-		}
-
-		return paginatedList;
+		
+		List<com.secondmarket.model.Company> testList;
+		CompanyDAOImpl c = new CompanyDAOImpl();
+		
+		testList=c.findAllCompanies();
+	    
+		Assert.assertFalse(testList.isEmpty());
+	
+	}
+	*/ 
+	/*public void testfindComoanyByName(){
+        
+		CompanyDAOImpl c = new CompanyDAOImpl();
+        com.secondmarket.model.Company test = c.findCompanyByName("Facebook");
+        //String companyname = test.getCompanyName();      
+      //  Assert.assertNotNull(test);	
+    */   
+	
+        
 	}
 
+
+
+
+
+
+
+
 	
 	
 	
 	
-}
+	
+
+
+
+
