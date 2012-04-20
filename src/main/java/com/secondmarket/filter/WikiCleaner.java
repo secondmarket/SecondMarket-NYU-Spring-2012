bc6bae3141fa294d0ff584eb69349242055d7cbf
Danjuan Ye <yedanjuan@gmail.com>
2012-04-20 10:53:28 -0400

    remove wiki markup(version 1)

--- src/main/java/com/secondmarket/filter/WikiCleaner.java
+++ src/main/java/com/secondmarket/filter/WikiCleaner.java
@@ -1,16 +1,38 @@
 package com.secondmarket.filter;
 
-import info.bliki.wiki.filter.PlainTextConverter;
-import info.bliki.wiki.model.WikiModel;
+import java.util.Map;
+
+import com.secondmarket.dao.CompanyDAO;
+import com.secondmarket.daoimpl.CompanyDAOImpl;
+import com.secondmarket.properties.SMProperties;
+import com.secondmarket.utility.DataMapper;
 
 public class WikiCleaner {
-	
-	public static final String TEST = "This is a [[Hello World]] '''example'''";
 
-    public static void main(String[] args) {
-            WikiModel wikiModel = new WikiModel("http://www.mywiki.com/wiki/${image}", "http://www.mywiki.com/wiki/${title}");
-            String plainStr = wikiModel.render(new PlainTextConverter(), TEST);
-            System.out.print(plainStr);
-    }
+	public CompanyDAO companyDao;
+	private SMProperties wikiProperty;
+
+	public WikiCleaner() {
+		try {
+			wikiProperty = SMProperties.getInstance("WIKI");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		companyDao = new CompanyDAOImpl(wikiProperty);
+	}
+
+	public static void main(String[] args) {
+		String title = "Wetpaint";
+		String url_Wikipedia = "http://en.wikipedia.org/w/api.php?action=query&titles="
+				+ title + "&prop=revisions&rvprop=content&format=json";
+
+		Map<String,String> wikipediaDoc = DataMapper.getDataInMapFromAPI(url_Wikipedia);
+
+		WikiCleaner test = new WikiCleaner();
+	// TODO pass one more map for wikipedia doc
+	    test.companyDao.saveCompany("Wetpaint",
+			null, wikipediaDoc, null, url_Wikipedia);
+	}
 
 }
